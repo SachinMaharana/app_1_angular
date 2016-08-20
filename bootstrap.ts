69deb676fad91bc3fdb22a6ce997873daf2f450b
Sachin <sachin.nicky@gmail.com>
2016-08-20 15:39:19 +0530

    Setting Up

---
+++ bootstrap.ts
@@ -0,0 +1,4 @@
+import { bootstrap } from '@angular/platform-browser-dynamic';
+import { PonyRacerAppComponent } from './ponyracer-app.component';
+
+bootstrap(PonyRacerAppComponent).catch(err => console.log(err));
